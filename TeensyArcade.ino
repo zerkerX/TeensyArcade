fb1f40957b192446317b1e8084d61a37a7fa4d8b
Ryan Armstrong <git@zerker.ca>
2018-12-30 12:37:02 -0500

    Updated for proper button assignments

--- TeensyArcade.ino
+++ TeensyArcade.ino
@@ -1,32 +1,85 @@
-int pins[] = {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    15, 16, 17, 18, 19, 20, 21, 22, 23
+//#include <stdbool.h>
+
+enum joystick_buttons {
+    STICK_LEFT = 0,
+    STICK_RIGHT,
+    STICK_UP,
+    STICK_DOWN,
+    BUTTON_BLUE,
+    BUTTON_GREEN,
+    BUTTON_RED,
+    BUTTON_YELLOW,
+    BUTTON_TOP_LEFT,
+    BUTTON_TOP_RIGHT,
+    BUTTON_PB_LEFT,
+    BUTTON_PB_RIGHT,
+    BUTTON_FRONT_WHITE,
+    BUTTON_FRONT_BLACK,
+    TOGGLE_PUSH,
+    TOGGLE_SWITCH_1,
+    TOGGLE_SWITCH_2,
+    NUM_BUTTONS
 };
 
-#define NUMPINS (sizeof(pins) / sizeof(int))
+#define SNES_LATCH 22
+#define SNES_CLOCK 23
+#define SNES_DATA 21
 
+int button_pins[NUM_BUTTONS] = 
+{
+    [STICK_LEFT] = 1,
+    [STICK_RIGHT] = 0,
+    [STICK_UP] = 3,
+    [STICK_DOWN] = 2,
+    [BUTTON_BLUE] = 16,
+    [BUTTON_GREEN] = 15,
+    [BUTTON_RED] = 17,
+    [BUTTON_YELLOW] = 19,
+    [BUTTON_TOP_LEFT] = 18,
+    [BUTTON_TOP_RIGHT] = 20,
+    [BUTTON_PB_LEFT] = 4,
+    [BUTTON_PB_RIGHT] = 7,
+    [BUTTON_FRONT_WHITE] = 5,
+    [BUTTON_FRONT_BLACK] = 6,
+    [TOGGLE_PUSH] = 9,
+    [TOGGLE_SWITCH_1] = 8,
+    [TOGGLE_SWITCH_2] = 10,
+};
+
+bool button_states[NUM_BUTTONS] = { [STICK_LEFT] = false };
 
 
 void setup()
 {
-    size_t i;
+    int button;
+
     Serial.begin(19200);
     
-    for (i = 0; i < NUMPINS; i++)
+    for (button = STICK_LEFT; button < NUM_BUTTONS; button++)
     {
-        pinMode(pins[i], INPUT_PULLUP);
+        pinMode(button_pins[button], INPUT_PULLUP);
     } 
+
+    pinMode(SNES_LATCH, INPUT_PULLUP);
+    pinMode(SNES_CLOCK, INPUT_PULLUP);
+    pinMode(SNES_DATA, OUTPUT);
 }
 
 void loop()
 {
-    size_t i;
+    int button;
 
-    for (i = 0; i < NUMPINS; i++)
+    for (button = STICK_LEFT; button < NUM_BUTTONS; button++)
     {
-        Serial.printf("%d: %d; ", pins[i], digitalRead(pins[i]));
+        button_states[button] = !digitalRead(button_pins[button]);
     }
-    Serial.println("");
+    
+    Serial.printf("UDLR: %d%d%d%d\r\n", 
+        button_states[STICK_UP],
+        button_states[STICK_DOWN],
+        button_states[STICK_LEFT],
+        button_states[STICK_RIGHT]
+        );
 
     delay(100);
 }
