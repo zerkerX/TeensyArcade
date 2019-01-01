5795f20686c769e2b25406fae46b6b4ee9f3aacb
Ryan Armstrong <git@zerker.ca>
2019-01-01 07:42:24 -0500

    Split Arcade stick features into its own class

---
+++ ArcadeStick.hpp
@@ -0,0 +1,72 @@
+class ArcadeStick
+{
+  public:
+    enum joystick_buttons {
+        STICK_LEFT = 0,
+        STICK_RIGHT,
+        STICK_UP,
+        STICK_DOWN,
+        BUTTON_BLUE,
+        BUTTON_GREEN,
+        BUTTON_RED,
+        BUTTON_YELLOW,
+        BUTTON_TOP_LEFT,
+        BUTTON_TOP_RIGHT,
+        BUTTON_PB_LEFT,
+        BUTTON_PB_RIGHT,
+        BUTTON_FRONT_WHITE,
+        BUTTON_FRONT_BLACK,
+        TOGGLE_PUSH,
+        TOGGLE_SWITCH_1,
+        TOGGLE_SWITCH_2,
+        NUM_BUTTONS
+    };
+
+    ArcadeStick()
+    {
+        for (int button = STICK_LEFT; button < NUM_BUTTONS; button++)
+        {
+            pinMode(button_pins[button], INPUT_PULLUP);
+        } 
+    }
+    
+    void load()
+    {
+        for (int button = STICK_LEFT; button < NUM_BUTTONS; button++)
+        {
+            button_states[button] = !digitalRead(button_pins[button]);
+        }
+    }
+    
+    inline bool button(joystick_buttons button)
+    {
+        return button_states[button];
+    }
+    
+   
+  private:
+
+    const int button_pins[NUM_BUTTONS] = 
+    {
+        1, // STICK_LEFT
+        0, // STICK_RIGHT
+        3, // STICK_UP
+        2, // STICK_DOWN
+        16, // BUTTON_BLUE
+        15, // BUTTON_GREEN
+        17, // BUTTON_RED
+        19, // BUTTON_YELLOW
+        18, // BUTTON_TOP_LEFT
+        20, // BUTTON_TOP_RIGHT
+        4, // BUTTON_PB_LEFT
+        7, // BUTTON_PB_RIGHT
+        5, // BUTTON_FRONT_WHITE
+        6, // BUTTON_FRONT_BLACK
+        9, // TOGGLE_PUSH
+        8, // TOGGLE_SWITCH_1
+        10, // TOGGLE_SWITCH_2
+    };
+
+    bool button_states[NUM_BUTTONS] = { false };
+    
+};
--- TeensyArcade.ino
+++ TeensyArcade.ino
@@ -1,64 +1,14 @@
-//#include <stdbool.h>
-
-enum joystick_buttons {
-    STICK_LEFT = 0,
-    STICK_RIGHT,
-    STICK_UP,
-    STICK_DOWN,
-    BUTTON_BLUE,
-    BUTTON_GREEN,
-    BUTTON_RED,
-    BUTTON_YELLOW,
-    BUTTON_TOP_LEFT,
-    BUTTON_TOP_RIGHT,
-    BUTTON_PB_LEFT,
-    BUTTON_PB_RIGHT,
-    BUTTON_FRONT_WHITE,
-    BUTTON_FRONT_BLACK,
-    TOGGLE_PUSH,
-    TOGGLE_SWITCH_1,
-    TOGGLE_SWITCH_2,
-    NUM_BUTTONS
-};
+#include "ArcadeStick.hpp"
 
 #define SNES_LATCH 22
 #define SNES_CLOCK 23
 #define SNES_DATA 21
 
-int button_pins[NUM_BUTTONS] = 
-{
-    [STICK_LEFT] = 1,
-    [STICK_RIGHT] = 0,
-    [STICK_UP] = 3,
-    [STICK_DOWN] = 2,
-    [BUTTON_BLUE] = 16,
-    [BUTTON_GREEN] = 15,
-    [BUTTON_RED] = 17,
-    [BUTTON_YELLOW] = 19,
-    [BUTTON_TOP_LEFT] = 18,
-    [BUTTON_TOP_RIGHT] = 20,
-    [BUTTON_PB_LEFT] = 4,
-    [BUTTON_PB_RIGHT] = 7,
-    [BUTTON_FRONT_WHITE] = 5,
-    [BUTTON_FRONT_BLACK] = 6,
-    [TOGGLE_PUSH] = 9,
-    [TOGGLE_SWITCH_1] = 8,
-    [TOGGLE_SWITCH_2] = 10,
-};
-
-bool button_states[NUM_BUTTONS] = { [STICK_LEFT] = false };
-
+ArcadeStick stick;
 
 void setup()
 {
-    int button;
-
-    Serial.begin(19200);
-    
-    for (button = STICK_LEFT; button < NUM_BUTTONS; button++)
-    {
-        pinMode(button_pins[button], INPUT_PULLUP);
-    } 
+    Joystick.useManualSend(true);
 
     pinMode(SNES_LATCH, INPUT_PULLUP);
     pinMode(SNES_CLOCK, INPUT_PULLUP);
@@ -67,36 +17,12 @@ void setup()
 
 void loop()
 {
-    int button;
+    stick.load();
+    Joystick.button(1, stick.button(ArcadeStick::BUTTON_BLUE));
+    Joystick.button(2, stick.button(ArcadeStick::BUTTON_GREEN));
+    Joystick.button(3, stick.button(ArcadeStick::BUTTON_RED));
 
-    for (button = STICK_LEFT; button < NUM_BUTTONS; button++)
-    {
-        button_states[button] = !digitalRead(button_pins[button]);
-    }
-    
-    Serial.printf("UDLR: %d%d%d%d, Top: %d%d%d%d%d%d, Side: %d%d, Front %d%d, Toggles: %d%d%d\r\n", 
-        button_states[STICK_UP],
-        button_states[STICK_DOWN],
-        button_states[STICK_LEFT],
-        button_states[STICK_RIGHT],
-
-        button_states[BUTTON_BLUE],
-        button_states[BUTTON_GREEN],
-        button_states[BUTTON_RED],
-        button_states[BUTTON_TOP_LEFT],
-        button_states[BUTTON_YELLOW],
-        button_states[BUTTON_TOP_RIGHT],
-
-        button_states[BUTTON_PB_LEFT],
-        button_states[BUTTON_PB_RIGHT],
-
-        button_states[BUTTON_FRONT_WHITE],
-        button_states[BUTTON_FRONT_BLACK],
-
-        button_states[TOGGLE_PUSH],
-        button_states[TOGGLE_SWITCH_1],
-        button_states[TOGGLE_SWITCH_2]
-        );
+    Joystick.send_now();
 
     delay(100);
 }
